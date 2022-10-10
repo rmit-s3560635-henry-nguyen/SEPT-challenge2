130a412fad41c34ea9d862ebfa441f27bef62cd9
Henry Nguyen <s3560635@student.rmit.edu.au>
2022-10-10 16:24:46 +1100

    switched to using JpaRepository instead

---
+++ person-service/src/main/java/com/example/personservice/controller/PersonController.java
@@ -0,0 +1,12 @@
+package com.example.personservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/person")
+public class PersonController {
+
+    //add service here
+
+}
